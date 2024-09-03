8ee04bf982fab24e192cc8b0f1d7f17dd9a386ec
Ghostcito <jmguticasani1254@gmail.com>
2024-09-02 19:28:09 -0500

    Cambios finales

---
+++ Controllers/NuevoController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Practica.Controllers
+{
+    [Route("[controller]")]
+    public class NuevoController : Controller
+    {
+        private readonly ILogger<NuevoController> _logger;
+
+        public NuevoController(ILogger<NuevoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ Models/Asginatura.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica.Models
+{
+    public class Asginatura
+    {
+        public string nombreAsignatura {get;set;}
+        public string asignaturaId {get;set;}
+    }
+}
