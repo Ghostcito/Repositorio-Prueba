76361d5d6475adf4a71c717f8d995d1ad832b430
Ghostcito <jmguticasani1254@gmail.com>
2024-09-02 19:07:02 -0500

    Segundo documento

---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica.Models
+{
+    public class Usuario
+    {
+        public string nombre {get;set;}
+        public string apellido {get;set;}
+    }
+}
