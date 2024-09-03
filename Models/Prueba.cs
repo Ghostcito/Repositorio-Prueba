0037584a000c7432c601b4ec24795e5cf761bb7b
Ghostcito <jmguticasani1254@gmail.com>
2024-09-02 19:55:38 -0500

    Mensajeeee

---
+++ Models/Prueba.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica.Models
+{
+    public class Prueba
+    {
+        public string nombrePrueba;
+    }
+}
